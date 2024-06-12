175471e121a29c1c9da552eaabb34084ea429c4d
Guru charan Battineni <battinenigurucharan@gmail.com>
2024-06-12 18:05:10 -0400

    navbar added

---
+++ Client/src/components/NavBar.jsx
@@ -0,0 +1,58 @@
+
+import React, { useState } from 'react';
+import {
+  AppBar,
+  Box,
+  Button,
+  Container,
+  IconButton,
+  Toolbar,
+  Typography,
+} from '@mui/material';
+import { Lock, Menu } from '@mui/icons-material';
+
+
+const NavBar = () => {
+ 
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <>
+      <AppBar>
+        <Container maxWidth="lg">
+          <Toolbar disableGutters>
+            <Box sx={{ mr: 1 }}>
+              <IconButton
+                size="large"
+                color="inherit"
+                onClick={() => setIsOpen(true)}
+              >
+                <Menu />
+              </IconButton>
+            </Box>
+            <Typography
+              variant="h6"
+              component="h1"
+              noWrap
+              sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}
+            >
+              You Are Welcome
+            </Typography>
+            <Typography
+              variant="h6"
+              component="h1"
+              noWrap
+              sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
+            >
+              YRW
+            </Typography>
+          
+          </Toolbar>
+        </Container>
+      </AppBar>
+      <Toolbar />
+    </>
+  );
+};
+
+export default NavBar;
