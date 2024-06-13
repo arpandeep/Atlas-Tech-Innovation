ff6ace5a0bae5281d49edec35d1d8313e1b44e20
Arpandeep <appsedeep@gmail.com>
2024-06-13 15:57:53 -0400

    Fix case sensitivity issue with .gitignore

---
+++ Client/src/components/NavBar.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { AppBar, Box, Button, Container, IconButton, Toolbar, Typography } from "@mui/material";
+import { Menu , Lock } from "@mui/icons-material";
+import { useValue } from "../context/ContextProvider";
+import UserIcons from "./user/UserIcons";
+import photoURL from './logo.jpeg'
+
+const user ={name:'test', photoURL}
+
+const NavBar = () => {
+
+  const{
+    state:{currentUser},
+    dispatch} = useValue();
+
+
+  return (
+    <AppBar>
+      <Container maxWidth='lg'>
+        <Toolbar disableGutters>
+          <Box sx={{ mr: 1 }}>
+            <IconButton size="large" color="inherit">
+              <Menu />
+            </IconButton>
+          </Box>
+          <Typography
+            variant="h6"
+            component='h1'
+            noWrap
+            sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}
+          >
+            Atlas Tech Innovation
+          </Typography>
+          <Typography
+            variant="h6"
+            component='h1'
+            noWrap
+            sx={{ display: { xs: 'flex', md: 'none' } }}
+          >
+            Atlas
+          </Typography>
+
+          {!currentUser ? 
+          (<Button color="inherit" startIcon={<Lock/>} onClick={()=> dispatch({type:'UPDATE_USER',payload:user})}  >
+            Login
+          </Button>) : (
+            <UserIcons />
+          )}
+
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+
+export default NavBar;
---
+++ Client/src/context/ContextProvider.js
@@ -0,0 +1,29 @@
+
+import React from "react";
+import { createContext} from "react";
+import { useContext } from "react";
+import { useReducer } from "react"; 
+import reducer from "./reducer";
+
+
+const initialState={
+    currentUser:null
+}
+
+const Context= createContext(initialState)
+
+export const useValue =() =>{
+    return useContext(Context)
+}
+
+const ContextProvider =({children}) =>{
+    const[state, dispatch]= useReducer(reducer, initialState)
+    return( 
+        <Context.Provider value={{state,dispatch}}>
+            {children}
+        </Context.Provider>
+
+    )
+}
+
+export default ContextProvider;
