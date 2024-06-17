72e51720bd7b2ccc75f4170a27876faf3b347031
Guru charan Battineni <battinenigurucharan@gmail.com>
2024-06-17 17:59:35 -0400

    Login and signup form

--- Client/src/context/reducer.js
+++ Client/src/context/reducer.js
@@ -1,13 +1,41 @@
 import React from "react";
 
-
-const reducer=(state, action)=>{
-    switch(action.type){
-        case 'UPDATE_USER':
-            return{...state,currentUser:action.payload}
-
-        default:
-            throw new Error("NO MATCCHED ACTION!")
+const reducer = (state, action) => {
+    switch (action.type) {
+      case 'UPDATE_ALERT':
+        return {
+          ...state,
+          alert: action.payload,
+        };
+      case 'OPEN_LOGIN':
+        return {
+          ...state,
+          openLogin: true,
+        };
+      case 'CLOSE_LOGIN':
+        return {
+          ...state,
+          openLogin: false,
+        };
+      case 'START_LOADING':
+        return {
+          ...state,
+          loading: true,
+        };
+      case 'END_LOADING':
+        return {
+          ...state,
+          loading: false,
+        };
+      case 'SET_CURRENT_USER':
+        return {
+          ...state,
+          currentUser: action.payload,
+        };
+      default:
+        return state;
     }
-}
-export default reducer;
+  };
+  
+  export default reducer;
+  
