be4f61b1572e2ac78a798bcae9c8fb6e1ce185d1
Guru charan Battineni <battinenigurucharan@gmail.com>
2024-06-20 14:52:27 -0400

    GoogleOneTapLogin

--- Client/src/components/NavBar.js
+++ Client/src/components/NavBar.js
@@ -1,22 +1,28 @@
-import React from "react";
-import { AppBar, Box, Button, Container, IconButton, Toolbar, Typography } from "@mui/material";
-import { Menu , Lock } from "@mui/icons-material";
-import { useValue } from "../context/ContextProvider";
-import UserIcons from "./user/UserIcons";
-import photoURL from './logo.jpeg'
 
-const user ={name:'test', photoURL}
+import React from 'react';
+import {
+  AppBar,
+  Box,
+  Button,
+  Container,
+  IconButton,
+  Toolbar,
+  Typography,
+} from '@mui/material';
+import { Lock, Menu } from '@mui/icons-material';
+
+import { useValue } from '../context/ContextProvider';
+import UserIcons from './user/UserIcons';
 
 const NavBar = () => {
-
-  const{
-    state:{currentUser},
-    dispatch} = useValue();
-
+  const {
+    state: { currentUser },
+    dispatch,
+  } = useValue();
 
   return (
     <AppBar>
-      <Container maxWidth='lg'>
+      <Container maxWidth="lg">
         <Toolbar disableGutters>
           <Box sx={{ mr: 1 }}>
             <IconButton size="large" color="inherit">
@@ -25,7 +31,7 @@ const NavBar = () => {
           </Box>
           <Typography
             variant="h6"
-            component='h1'
+            component="h1"
             noWrap
             sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}
           >
@@ -33,25 +39,27 @@ const NavBar = () => {
           </Typography>
           <Typography
             variant="h6"
-            component='h1'
+            component="h1"
             noWrap
-            sx={{ display: { xs: 'flex', md: 'none' } }}
+            sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
           >
             Atlas
           </Typography>
-
-          {!currentUser ? 
-          (<Button color="inherit" startIcon={<Lock/>} onClick={()=> dispatch({type:'OPEN_LOGIN'})}  >
-            Login
-          </Button>) : (
+          {!currentUser ? (
+            <Button
+              color="inherit"
+              startIcon={<Lock />}
+              onClick={() => dispatch({ type: 'OPEN_LOGIN' })}
+            >
+              Login
+            </Button>
+          ) : (
             <UserIcons />
           )}
-
         </Toolbar>
       </Container>
     </AppBar>
   );
-}
+};
 
 export default NavBar;
-
