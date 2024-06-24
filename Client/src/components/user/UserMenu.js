f239ad45ffba5af58a56af5856f3780df54f3173
SaiVenu1018 <saivenu1018@gmail.com>
2024-06-24 18:04:18 -0400

    sai

--- Client/src/components/user/UserMenu.js
+++ Client/src/components/user/UserMenu.js
@@ -5,11 +5,39 @@ import React from "react";
 import { useValue } from "../../context/ContextProvider";
 const UserMenu =({anchorUserMenu,setAnchorUserMenu}) =>{
 
-    const {dispatch}=useValue();
+    const {dispatch,state: { currentUser }}=useValue();
 
 
     const handleCloseUserMenu =()=>{
         setAnchorUserMenu(null)
+    };
+    const testAuthorization = async () => {
+        const url = process.env.REACT_APP_SERVER_URL + '/room';
+        try {
+          const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+              authorization: `Bearer ${currentUser.token}t`,
+            }
+          })
+          const data = await response.json();
+
+      console.log(data);
+      if (!data.success) {
+        if (response.status === 401)
+          dispatch({ type: 'UPDATE_USER', payload: null });
+        throw new Error(data.message);
+      }
+        }
+        catch(error){
+            dispatch({
+                type: 'UPDATE_ALERT',
+                payload: { open: true, severity: 'error', message: error.message },
+              });
+              console.log(error);
+
+        }
     }
     return (
         <Menu
@@ -18,7 +46,7 @@ const UserMenu =({anchorUserMenu,setAnchorUserMenu}) =>{
         onClose={handleCloseUserMenu}
         onClick={handleCloseUserMenu}
         >
-            <MenuItem>
+            <MenuItem onClick={testAuthorization}>
             <ListItemIcon>
                 <Settings fontSize="small"/>
             </ListItemIcon>
