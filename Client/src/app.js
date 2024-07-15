7a92a79493ec206037edbdd158aa2495273b99af
SaiVenu1018 <saivenu1018@gmail.com>
2024-07-15 11:47:43 -0400

    dashboardcreation

--- Client/src/app.js
+++ Client/src/app.js
@@ -1,18 +1,16 @@
-import React from "react";
-import NavBar from "./components/NavBar";
-import Login from './components/user/Login'
-import Notification from "./components/Notification";
-import Loading from "./components/Loading";
-import BottomNav from "./components/BottomNav";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Dashboard from './pages/dashboard/Dashboard';
+import Home from './pages/Home';
 
 const App = () => {
   return (
     <>
-    <Loading />
-    <Notification />
-    <Login />
-    <NavBar />
-    <BottomNav/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="dashboard/*" element={<Dashboard />} />
+          <Route path="*" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 };
