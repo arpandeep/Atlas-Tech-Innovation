21fffb4bc3cc91c1b657cc1c9ccc39111bdcc565
Arpandeep <appsedeep@gmail.com>
2024-07-18 19:37:47 -0400

    changed added in login and signup for validations

--- Atlas/client/src/components/user/PasswordField.js
+++ Atlas/client/src/components/user/PasswordField.js
@@ -1,3 +1,50 @@
+// import { Visibility, VisibilityOff } from '@mui/icons-material';
+// import { IconButton, InputAdornment, TextField } from '@mui/material';
+// import React, { useState } from 'react';
+
+// const PasswordField = ({
+//   passwordRef,
+//   id = 'password',
+//   label = 'Password',
+// }) => {
+//   const [showPassword, setShowPassword] = useState(false);
+
+//   const handleClick = () => {
+//     setShowPassword(!showPassword);
+//   };
+
+//   const handleMouseDown = (e) => {
+//     e.preventDefault();
+//   };
+
+//   return (
+//     <TextField
+//       margin="normal"
+//       variant="standard"
+//       id={id}
+//       label={label}
+//       type={showPassword ? 'text' : 'password'}
+//       fullWidth
+//       inputRef={passwordRef}
+//       inputProps={{ minLength: 6 }}
+//       required
+//       InputProps={{
+//         endAdornment: (
+//           <InputAdornment position="end">
+//             <IconButton onClick={handleClick} onMouseDown={handleMouseDown}>
+//               {showPassword ? <VisibilityOff /> : <Visibility />}
+//             </IconButton>
+//           </InputAdornment>
+//         ),
+//       }}
+//     />
+//   );
+// };
+
+// export default PasswordField;
+
+
+
 import { Visibility, VisibilityOff } from '@mui/icons-material';
 import { IconButton, InputAdornment, TextField } from '@mui/material';
 import React, { useState } from 'react';
@@ -6,6 +53,8 @@ const PasswordField = ({
   passwordRef,
   id = 'password',
   label = 'Password',
+  error,
+  helperText,
 }) => {
   const [showPassword, setShowPassword] = useState(false);
 
@@ -28,6 +77,8 @@ const PasswordField = ({
       inputRef={passwordRef}
       inputProps={{ minLength: 6 }}
       required
+      error={error}
+      helperText={helperText}
       InputProps={{
         endAdornment: (
           <InputAdornment position="end">
